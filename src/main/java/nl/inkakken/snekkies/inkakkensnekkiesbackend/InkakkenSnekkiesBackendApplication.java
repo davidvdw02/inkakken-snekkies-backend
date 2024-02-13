018a428d05b358004a336188118723da907f4748
davidvdw02 <davidvdw02@gmail.com>
2024-02-13 13:58:29 +0100

    first push

---
+++ src/main/java/nl/inkakken/snekkies/inkakkensnekkiesbackend/InkakkenSnekkiesBackendApplication.java
@@ -0,0 +1,13 @@
+package nl.inkakken.snekkies.inkakkensnekkiesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InkakkenSnekkiesBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InkakkenSnekkiesBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/inkakken/snekkies/inkakkensnekkiesbackend/InkakkenSnekkiesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.inkakken.snekkies.inkakkensnekkiesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InkakkenSnekkiesBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
