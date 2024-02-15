2794f7bd84c1dbedf08ab75e7bb2759c3499ae68
davidvdw02 <davidvdw02@gmail.com>
2024-02-15 16:46:32 +0100

    Add CorsConfig and Attendee handling

---
+++ src/main/java/nl/inkakken/snekkies/inkakkensnekkiesbackend/repositories/AttendeeRepository.java
@@ -0,0 +1,13 @@
+package nl.inkakken.snekkies.inkakkensnekkiesbackend.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import nl.inkakken.snekkies.inkakkensnekkiesbackend.models.Attendee;
+
+@Repository
+public interface AttendeeRepository extends JpaRepository<Attendee, UUID>{
+    
+}
---
+++ src/main/java/nl/inkakken/snekkies/inkakkensnekkiesbackend/services/AttendeeService.java
@@ -0,0 +1,51 @@
+package nl.inkakken.snekkies.inkakkensnekkiesbackend.services;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import nl.inkakken.snekkies.inkakkensnekkiesbackend.repositories.AttendeeRepository;
+import nl.inkakken.snekkies.inkakkensnekkiesbackend.models.Attendee;
+@Service
+public class AttendeeService {
+
+
+        private final Logger logger = LoggerFactory.getLogger(OnlineRecipeService.class);
+
+     private final AttendeeRepository attendeeRepository;
+
+    @Autowired
+    public AttendeeService(AttendeeRepository attendeeRepository) {
+        this.attendeeRepository = attendeeRepository;
+    }
+
+    public List<Attendee> getAllAttendees() {
+        logger.debug("Getting all attendees");
+        return attendeeRepository.findAll();
+    }
+
+    public Attendee getAttendeeById(UUID id) {
+        logger.debug("Getting attendee with id: " + id);
+        Attendee attendee = attendeeRepository.findById(id).orElseThrow(() -> {
+            logger.error("Attendee not found with id: " + id);
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Attendee not found with id: " + id);
+        });
+        return attendee;
+    }
+
+    public Attendee createAttendee(Attendee attendee) {
+        logger.debug("Creating attendee");
+        return attendeeRepository.save(attendee);
+    }
+
+    public void deleteAttendee(UUID id) {
+        logger.debug("Deleting attendee with id: " + id);
+        attendeeRepository.deleteById(id);
+    }
+}
