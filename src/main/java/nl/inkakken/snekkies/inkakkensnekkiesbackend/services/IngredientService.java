2c2ed9b63b96fd83fc1402ff608788ae9cc81ecf
davidvdw02 <davidvdw02@gmail.com>
2024-02-20 16:29:36 +0100

    fixed empty methods in ingredientService

--- src/main/java/nl/inkakken/snekkies/inkakkensnekkiesbackend/services/IngredientService.java
+++ src/main/java/nl/inkakken/snekkies/inkakkensnekkiesbackend/services/IngredientService.java
@@ -4,7 +4,11 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import nl.inkakken.snekkies.inkakkensnekkiesbackend.models.Ingredient;
 import nl.inkakken.snekkies.inkakkensnekkiesbackend.repositories.IngredientRepository;
@@ -13,34 +17,27 @@ import nl.inkakken.snekkies.inkakkensnekkiesbackend.repositories.IngredientRepos
 public class IngredientService {
 
 
+    private final Logger logger = LoggerFactory.getLogger(IngredientService.class);
     private final IngredientRepository ingredientRepository;
 
     public IngredientService(IngredientRepository ingredientRepository) {
         this.ingredientRepository = ingredientRepository;
     }
 
-
-    public void createIngredient(Ingredient ingredient) {
-        // Implementation goes here
-    }
-
     public Ingredient getIngredientById(UUID id) {
-        // Implementation goes here
-        return null;
+      return this.ingredientRepository.findById(id).orElseThrow(() -> {
+            logger.error("Ingredient not found with id: " + id);
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Ingredient not found with id: " + id);
+        });
     }
 
     public List<Ingredient> getAllIngredients() {
+        logger.debug("Getting all ingredients");
         return this.ingredientRepository.findAll();
     }
-
-    public void updateIngredient(Ingredient ingredient) {
-        // Implementation goes here
-    }
-
-    public void deleteIngredient(int id) {
-        // Implementation goes here
-    }
+    
     public Ingredient postNewIngredient(Ingredient newIngredientRequest) {
+        logger.debug("Creating ingredient");
         Optional<Ingredient> existingIngredient = this.ingredientRepository.findByName(newIngredientRequest.getName());
        if(existingIngredient.isEmpty()){
         return this.ingredientRepository.save(newIngredientRequest);
