576e42602b652aecd0474cebb56724c58d22b07f
davidvdw02 <davidvdw02@gmail.com>
2024-03-01 20:08:43 +0100

    after alot of reading we dont need the @SpringBootTest

--- src/test/java/nl/inkakken/snekkies/inkakkensnekkiesbackend/InkakkenSnekkiesBackendApplicationTests.java
+++ src/test/java/nl/inkakken/snekkies/inkakkensnekkiesbackend/InkakkenSnekkiesBackendApplicationTests.java
@@ -1,9 +1,7 @@
 package nl.inkakken.snekkies.inkakkensnekkiesbackend;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class InkakkenSnekkiesBackendApplicationTests {
 
     @Test
